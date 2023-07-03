9dfe2675f828c7ec8bb3ce8e6dbe8533f24ff6d7
DenisGultsev <135717609+DenisGultsev@users.noreply.github.com>
2023-07-04 05:20:18 +1000

    Task 001 Degree Of

---
+++ Task001_Degree _Of/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Write("Введите число A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+while (a == 0)
+{
+    Console.WriteLine("Ошибка: число A должно быть натуральным числом.");
+    Console.Write("Пожалуйста, введите число A заново: ");
+    a = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.Write("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+while (b == 0)
+{
+    Console.WriteLine("Ошибка: число B должно быть натуральным числом.");
+    Console.Write("Пожалуйста, введите число B заново: ");
+    b = Convert.ToInt32(Console.ReadLine());
+}
+
+int result = a;
+for (int i = 1; i < b; i++)
+{
+    result *= a;
+}
+Console.WriteLine($"{a}, {b} → {result} ({a}ⁿ), где n = {b}.");
